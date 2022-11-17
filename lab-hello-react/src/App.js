b1f234a7233f834e67f9e4102f6ffdc5110dc04a
Wiebke Hagenauer <wiebosaurus@hotmail.com>
2022-11-17 18:28:53 +0100

    Iteration 1–4.5

--- lab-hello-react/src/App.js
+++ lab-hello-react/src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
 
+import logo from "./images/ironhack-logo-xs.png";
+import bar from "./images/menu-top-xs.png";
+import img1 from "./images/icon1.png";
+import img2 from "./images/icon2.png";
+import img3 from "./images/icon3.png";
+import img4 from "./images/icon4.png";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <img src={logo} alt="ironhack-logo" />
+      <img src={bar} alt="menu-bar" />
+      <h1>Say hello to ReactJS</h1>
+      <p>
+        You will learn how to use the most popular frontend library, and become
+        a super Ninja developer
+      </p>
+      <button>Awesome!</button>
+      <img src={img1} alt="ironhack-icon1" />
+      <img src={img2} alt="ironhack-icon2" />
+      <img src={img3} alt="ironhack-icon3" />
+      <img src={img4} alt="ironhack-icon4" />
+    </div>
+  );
 }
 
 export default App;
